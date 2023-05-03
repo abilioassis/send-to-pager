38b6035a6b31f7fe0cf885502a53cf2aee5f3e24
Abilio de Assis <abilio.assis@petrobras.com.br>
2023-05-03 16:34:39 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,23 @@
+# Scrimba Bootcamp - Solo Projects
+
+This is a solution to the [M3 - Send to Pager](https://scrimba.com/scrim/co9b447f7b7a0dc6201d27636).
+
+## Screenshot
+
+![](img/screenshot.png)
+
+## Links
+
+- Live Site: [Netlify]()
+- Codebase: [Github]()
+- Code review: [Scrimba]()
+
+## Built with
+
+- Semantic HTML5 markup.
+- Flexbox.
+- JavaScript (no frameworks).
+
+## Author
+
+- [Abilio Assis](https://www.linkedin.com/in/abilio-assis/)
---
+++ js/index.js
@@ -0,0 +1,52 @@
+"use strict";
+
+const pagerMsgEl = document.getElementById("pager-msg");
+const phoneNumberEl = document.getElementById("phone-number");
+const statusEl = document.getElementById("status");
+
+addEventListenerToButtons();
+
+function addEventListenerToButtons() {
+  const btnsEl = document.getElementsByClassName("btn");
+  const length = btnsEl.length;
+  // assigning event handlers to buttons
+  for (let idx = 0; idx < length; idx++) {
+    const btnEl = btnsEl[idx];
+    // check if this element has the 'reset' class
+    if (btnEl.classList.contains("reset")) {
+      // add the correct handler
+      btnEl.addEventListener("click", function () {
+        resetDisplays();
+      });
+      // check if this element has the 'send' class
+    } else if (btnEl.classList.contains("send")) {
+      // add the correct handler
+      btnEl.addEventListener("click", function () {
+        statusEl.style.visibility = "visible";
+        setTimeout(sendToPager, 2000);
+      });
+    } else {
+      // has the 'key' class
+      btnEl.addEventListener("click", function () {
+        // add the correct handler
+        updatePhoneNumber(this);
+      });
+    }
+  }
+}
+
+function resetDisplays() {
+  pagerMsgEl.innerHTML = "Relax";
+  phoneNumberEl.innerHTML = "";
+}
+
+function sendToPager() {
+  const audio = new Audio("sound.wav");
+  audio.play();
+  pagerMsgEl.innerHTML = phoneNumberEl.innerHTML;
+  statusEl.style.visibility = "hidden";
+}
+
+function updatePhoneNumber(btn) {
+  phoneNumberEl.innerHTML += btn.innerText;
+}
